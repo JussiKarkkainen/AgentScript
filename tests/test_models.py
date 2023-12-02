becb56a55cf2b49c9386b1a5f85996067402e4d7
Jussi Karkkainen <juskark@gmail.com>
2023-12-02 21:58:56 +0200

    model test

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,8 +3,15 @@ from configs.config import TestCarRacingConfig
 import numpy as np
 from tinygrad.tensor import Tensor
 
-def test_vae_shapes():
+def test_vae_input_shape():
     vae = models.SimpleVarAutoEnc(TestCarRacingConfig["perception"])
     sample_input = Tensor(np.random.randn(1, 3, 64, 64))
     z = vae.encode(sample_input)
     assert z.shape == (1, 32)
+
+
+def test_vae_output_shape():
+    vae = models.SimpleVarAutoEnc(TestCarRacingConfig["perception"])
+    z = Tensor.randn(1, 32)
+    out = vae.decode(z)
+    assert out.shape == (1, 3, 64, 64)
