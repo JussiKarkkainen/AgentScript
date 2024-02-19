0d2075c32b0f8b1c3cf883fe93bdb922c16f2024
Jussi Karkkainen <juskark@gmail.com>
2024-02-19 21:13:09 +0200

    make_target()

--- libcore/replay_buffer.py
+++ libcore/replay_buffer.py
@@ -24,12 +24,27 @@ class ReplayBuffer:
     def sample(self, batch_size: int, num_unroll_steps: int, td_steps: int) -> List[Experience]:
         episodes = random.sample(self.buffer, batch_size)
         positions = [(episode, random.randint(0, max(0, (len(episode.actions) - num_unroll_steps - 1)))) for episode in episodes]
-        batch = [(episode.obs[i], eoisode.actions[i:i+num_unroll_steps], self.make_target(i, num_unroll_steps, td_steps, episode) for episode, i in positions]
-        
-        raise Exception
-        return random.sample(self.buffer, batch_size)
+        batch = [(episode.obs[i], episode.actions[i:i+num_unroll_steps], self.make_target(i, num_unroll_steps, td_steps, episode)) for episode, i in positions]
+        return batch
     
     def make_target(self, i, num_unroll_steps, td_steps, episode):
+        targets = []
+        for idx in range(i, i+num_unroll_steps+1):
+            bootstrap_index = idx + td_steps
+            if bootstrap_index < len(episode.root_values):
+                value = episode.root_values[bootstrap_index] * config["discount"]**td_steps
+            else:
+                value = 0
+            for i, reward in enumerate(episode.rewards[idx:bootstrap_index]):
+                value += reward * config["discount"]**i
+            if idx > 0 and idx <= len(episode.rewards):
+                last_reward = episode.rewards[idx - 1]
+            else:
+                last_reward = 0
+            if current_index < len(self.root_values):
+                targets.append((value, last_reward, self.child_visits[current_index]))
+            else:
+                targets.append((0, last_reward, []))
         return targets
 
     def __len__(self):
